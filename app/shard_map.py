59e538fbc75e79c705137b48ac332767bc46b789
vishalsharma1996 <vishalg148256@gmail.com>
2025-12-01 16:08:44 +0000

    Added exhaustive list of shard map

--- app/shard_map.py
+++ app/shard_map.py
@@ -1,32 +1,72 @@
 shard_map = {
-    # TECH
+    # ------------------ TECH ------------------
     "msft": "shard_tech",
+    "microsoft": "shard_tech",
+
     "adbe": "shard_tech",
+    "adobe": "shard_tech",
+
     "orcl": "shard_tech",
+    "oracle": "shard_tech",
+
     "nvda": "shard_tech",
+    "nvidia": "shard_tech",
+
     "amzn": "shard_tech",
+    "amazon": "shard_tech",
+
     "aapl": "shard_tech",
+    "apple": "shard_tech",
+    "appl": "shard_tech",
+
     "googl": "shard_tech",
+    "google": "shard_tech",
+    "alphabet": "shard_tech",
+
     "meta": "shard_tech",
 
-    # CONSUMER / RETAIL / AUTO
+
+    # ------------ CONSUMER / RETAIL / AUTO ------------
     "nflx": "shard_consumer",
+    "netflix": "shard_consumer",
+
     "cpng": "shard_consumer",
-    "hd":   "shard_consumer",
+    "coupang": "shard_consumer",
+
+    "hd": "shard_consumer",
+    "home": "shard_consumer",
+
     "tsla": "shard_consumer",
+    "tesla": "shard_consumer",
 
-    # FINANCE
-    "jpm":  "shard_finance",
-    "v":    "shard_finance",
+
+    # ------------------ FINANCE ------------------
+    "jpm": "shard_finance",
+    "jp": "shard_finance",
+
+    "v": "shard_finance",
+    "visa": "shard_finance",
+
     "brka": "shard_finance",
+    "berkshire": "shard_finance",
 
-    # HEALTHCARE
-    "jnj":  "shard_healthcare",
-    "unh":  "shard_healthcare",
 
-    # INDUSTRIALS
-    "dal":  "shard_industrials",
+    # ------------------ HEALTHCARE ------------------
+    "jnj": "shard_healthcare",
+    "johnson": "shard_healthcare",
 
-    # CONSUMER STAPLES
-    "pg":   "shard_staples",
+    "unh": "shard_healthcare",
+    "unitedhealth": "shard_healthcare",
+
+
+    # ------------------ INDUSTRIALS ------------------
+    "dal": "shard_industrials",
+    "delta": "shard_industrials",
+
+
+    # ------------------ CONSUMER STAPLES ------------------
+    "pg": "shard_staples",
+    "procter": "shard_staples",   # optional
+    "procter&gamble": "shard_staples",
+    "gamble": "shard_staples"
 }
