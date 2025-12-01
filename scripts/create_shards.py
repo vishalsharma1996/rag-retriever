8edb3502b08ea8031874fbc46d6acccd1f0f9ac3
vishalsharma1996 <vishalg148256@gmail.com>
2025-12-01 15:37:31 +0000

    Adding code for initalizing persistent chroma in shard store

--- scripts/create_shards.py
+++ scripts/create_shards.py
@@ -1,8 +1,9 @@
 from chromadb import Client
+import chromadb
 from app.shard_map import shard_map
 def create_shard_collections():
-  '''Creates empty shard collection'''
-  chroma = Client()
+  '''Creates empty shard collection in shard_store'''
+  chroma = chromadb.PersistentClient(path="shard_store")
   shard_names = set(shard_map.values())
   for name in shard_names:
     chroma.get_or_create_collection(name)
