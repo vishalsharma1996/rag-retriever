f08d2f924be7368c2bebb8dd58f26ab63a926156
vishalsharma1996 <vishalg148256@gmail.com>
2025-12-01 13:18:37 +0000

    Adding shard map for chromadb

---
+++ app/shard_map.py
@@ -0,0 +1,32 @@
+shard_map = {
+    # TECH
+    "msft": "shard_tech",
+    "adbe": "shard_tech",
+    "orcl": "shard_tech",
+    "nvda": "shard_tech",
+    "amzn": "shard_tech",
+    "aapl": "shard_tech",
+    "googl": "shard_tech",
+    "meta": "shard_tech",
+
+    # CONSUMER / RETAIL / AUTO
+    "nflx": "shard_consumer",
+    "cpng": "shard_consumer",
+    "hd":   "shard_consumer",
+    "tsla": "shard_consumer",
+
+    # FINANCE
+    "jpm":  "shard_finance",
+    "v":    "shard_finance",
+    "brka": "shard_finance",
+
+    # HEALTHCARE
+    "jnj":  "shard_healthcare",
+    "unh":  "shard_healthcare",
+
+    # INDUSTRIALS
+    "dal":  "shard_industrials",
+
+    # CONSUMER STAPLES
+    "pg":   "shard_staples",
+}
